f5772b1b305e890fb894185a3d9afe2f300162f5
cloudjunc <32640689+cloudjunc@users.noreply.github.com>
2017-10-24 23:52:13 +0800

    mysql access

--- blogWeb/src/jDemo/UserServlet.java
+++ blogWeb/src/jDemo/UserServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
 @WebServlet(name = "UserServlet")
 public class UserServlet extends HttpServlet {
@@ -17,8 +18,58 @@ public class UserServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("text/html");
-        PrintWriter pw=response.getWriter();
-        pw.write("<h1>hello  first User!</h1>");
+//        response.setContentType("text/html");
+//        PrintWriter pw=response.getWriter();
+//        pw.write("<h1>hello  first User!</h1>");
+
+        Connection con;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "root";
+        String password = "123456";
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url,user,password);
+            if(!con.isClosed())
+                System.out.println("Succeeded connecting to the Database!");
+            Statement statement = con.createStatement();
+            String sql = "SELECT * FROM blogreport";
+            ResultSet rs = statement.executeQuery(sql);
+            String name = null;
+            String blogText = null;
+            while(rs.next())
+            {
+                //获取stuname这列数据
+                name = rs.getString("writer");
+                //获取stuid这列数据
+                blogText = rs.getString("content");
+                //首先使用ISO-8859-1字符集将name解码为字节序列并将结果存储新的字节数组中。
+                //然后使用GB2312字符集解码指定的字节数组。
+                name = new String(name.getBytes("ISO-8859-1"),"gb2312");
+                //输出结果
+                System.out.println(name + "\t" + blogText);
+            }
+            rs.close();
+            con.close();
+        }
+        catch(ClassNotFoundException e)
+        {
+            //数据库驱动类异常处理
+            System.out.println("Sorry,can`t find the Driver!");
+            e.printStackTrace();
+        }catch(SQLException e)
+        {
+            //数据库连接失败异常处理
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        finally
+        {
+            System.out.println("数据库数据成功获取！！");
+        }
     }
 }
